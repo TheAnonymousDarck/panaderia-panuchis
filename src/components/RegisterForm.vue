8b74ec5377017d31c9e2b738d2846fd3901eb29f
Mario Adair <programario12@gmail.com>
2022-06-10 02:51:45 -0500

    Validación formularios

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -4,31 +4,31 @@
     <ion-item>
         <ion-icon slot="start" :icon="personOutline"></ion-icon>
         <ion-label position="floating">Nombre completo</ion-label>
-        <ion-input v-model="authStore.nombre"></ion-input>
+        <ion-input v-model="authStore.nombre" minlength="3" required="true"></ion-input>
     </ion-item>
     
     <ion-item>
         <ion-icon slot="start" :icon="mailOutline"></ion-icon>
         <ion-label position="floating">Correo electronico</ion-label>
-        <ion-input type="email" v-model="authStore.email"></ion-input>
+        <ion-input type="email" v-model="authStore.email" required="true"></ion-input>
     </ion-item>
 
     <ion-item>
         <ion-icon slot="start" :icon="locationOutline"></ion-icon>
         <ion-label position="floating">Dirección</ion-label>
-        <ion-input v-model="authStore.direccion" ></ion-input>
+        <ion-input minlength="5" v-model="authStore.direccion" required="true"></ion-input>
     </ion-item>
 
     <ion-item>
         <ion-icon slot="start" :icon="callOutline"></ion-icon>
         <ion-label position="floating">Telefono</ion-label>
-        <ion-input type="tel" v-model="authStore.telefono"></ion-input>
+        <ion-input type="tel" placeholder="4421081217" minlength="10" v-model="authStore.telefono" required="true"></ion-input>
     </ion-item>
 
     <ion-item>
         <ion-icon :icon="keyOutline" slot="start"></ion-icon>
         <ion-label position="floating">Contraseña</ion-label>
-        <ion-input type="tel" v-model="authStore.password"></ion-input>
+        <ion-input type="password" minlength="6" v-model="authStore.password" required="true"></ion-input>
     </ion-item>
 
     <ion-button type="submit" expand="block" color="success" shape="round">
